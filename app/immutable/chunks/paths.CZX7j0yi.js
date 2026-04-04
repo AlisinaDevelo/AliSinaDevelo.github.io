5a3791baaae3e2593624201c296814fe47adad7c
AliSinaDevelo <AliSinaDevelo@users.noreply.github.com>
2026-04-04 02:31:26 +0000

    deploy: 30a4f02772f3f39f908caa942631e97944d1001a

---
+++ app/immutable/chunks/paths.CZX7j0yi.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1x6hf02)==null?void 0:s.base)??"/AliSinaDevelo.github.io";var e;const t=((e=globalThis.__sveltekit_1x6hf02)==null?void 0:e.assets)??a;export{t as a,a as b};
